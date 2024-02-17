d7a77366df4b79e26855dfeeb42e6b3bf5b18a1c
KarthikBalaji7 <karthikb644@gmail.com>
2024-02-17 21:44:37 +0530

    meslo font installation script

---
+++ meslo_font_installation_script.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+if [ $(uname)="Linux" ]
+then 
+	echo "Running Linux"
+	#font_dir="$HOME/.local/share/fonts"
+	font_dir="$HOME/bashTest/fontInstall/fonts"
+else 
+	echo "Running Mac"
+	font_dir="$HOME/Library/Fonts"
+fi 
+
+mkdir $font_dir
+
+fontName="Meslo"
+
+echo "Fonts directory created ::: $font_dir"
+
+curl https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip -L -o $font_dir/$fontName.zip
+
+unzip -q $font_dir/$fontName.zip -d $font_dir 
+
+rm $font_dir/$fontName.zip
